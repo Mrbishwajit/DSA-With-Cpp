ccf1b10b71318aa871b2d0c8eb1f78b54cfe0a6a
Mrbishwajit <bp30523052@gmail.com>
2025-07-11 00:01:47 +0530

    SORTING
    Inbuilt

--- Basic sorting algorithm/in built sort by c++.cpp
+++ Basic sorting algorithm/in built sort by c++.cpp
@@ -8,7 +8,15 @@ void print(int arr[],int n){
 }
     int main(){
         int arr[8]={1,4,1,3,2,4,3,7};
-       sort(arr,arr+8);
+           //For ascending order
+           cout<<"Ascendingorder=" ;
+           sort(arr,arr+8);
+           print(arr,8);
+
+               cout<<endl;
+        //For Descending order
+         cout<<"Descendingorder=" ;
+         sort(arr,arr+8,greater<int>());
 
         print(arr,8);
         return 0;
